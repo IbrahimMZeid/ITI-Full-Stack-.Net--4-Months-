21fc6fa900c43609663ab529a14547ba37b5b8f1
IbrahimMZeid <abrahymzyd43@gmail.com>
2025-03-24 15:29:56 +0200

    Day 4 add auto plau to slide task(t3)

--- JavaScript/Day 4/scripts/t3.js
+++ JavaScript/Day 4/scripts/t3.js
@@ -1,6 +1,6 @@
 var image = document.images[0];
 var cnt = 1;
-
+var interval;
 function prev() {
     cnt = (cnt == 1 ?6:cnt-1);
     image.src = ("images/"+cnt+".jpg");
@@ -8,4 +8,10 @@ function prev() {
 function next() {
     cnt = (cnt == 6 ?1:cnt+1);
     image.src = ("images/"+cnt+".jpg");
+}
+function play() {
+    interval = setInterval(next,2000);
+}
+function stop() {
+    clearInterval(interval);
 }
